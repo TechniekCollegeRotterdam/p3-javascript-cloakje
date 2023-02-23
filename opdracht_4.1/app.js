15efee97ebba8c437d0d1699910af4e124150005
Bart <9019875@student.zadkine.nl>
2023-02-23 11:19:52 +0100

    4.2

--- opdracht_4.1/app.js
+++ opdracht_4.1/app.js
@@ -2,23 +2,27 @@ let answer = document.getElementById("answer");
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
 
-function  mutliply() {
-
-answer.innerText=input1.value * input2.value;    
-
+function multiply(){
+    answer.innerText = input1.value * input2.value;
+    if(input1.value <= 0 || input2.value <= 0){
+    answer.innerText = "Het antwoord is te laag";
+    }
 }
-function  minus() {
-    answer.innerText=input1.value - input2.value;  
-
+function devide(){
+    answer.innerText = input1.value / input2.value;
+    if(input1.value <= 0 || input2.value <= 0){
+        answer.innerText = "Het antwoord is te laag";
+    }
 }
-function divide() {
-     
-    answer.innerText=input1.value / input2.value;
-
+function add(){
+    answer.innerText = Number(input1.value) + Number(input2.value);
+    if(input1.value <= 0 || input2.value <= 0){
+        answer.innerText = "Het antwoord is te laag";
+    }
 }
-function add() {
-    
-    answer.innerText= Number(input1.value) + Number(input2.value);
-
+function minus(){
+    answer.innerText = input1.value - input2.value;
+    if(input1.value <= 0 || input2.value <= 0){
+        answer.innerText = "Het antwoord is te laag";
+    }
 }
-x
