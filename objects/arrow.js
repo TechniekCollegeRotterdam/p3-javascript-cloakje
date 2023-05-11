f5576dabc300dedd3aa539c054781048218b232d
Bart <9019875@student.zadkine.nl>
2023-05-11 10:07:45 +0200

    arrow/loop

---
+++ objects/arrow.js
@@ -0,0 +1,51 @@
+
+
+//function calculate (a, b) {
+//let answer = a * b;
+//console.log(answer);
+//}
+
+//calculate(5, 15);
+
+//const calculate = (a, b) => {
+//    let answer(a, b);
+//    console.log(answer);
+//};
+
+//calculate(5, 15);
+
+
+//let name = "Bart";
+
+//let text = "Mijn naam is " + name;
+
+//let text = `mijn naam is ${name}`;
+
+//document.getElementById("output").innerText = text;
+//document.querySelector("#output").innerText = text;
+
+
+//const cars = ['bmw', 'audi', 'mercedes'];
+
+//for(let i = 0; i > cars.length; i++){
+//console.log(cars[i])
+// }
+
+// for(let car of cars){
+//     console.log(car)
+// }
+
+let marvels = ['spoderman', 'frozen', 'Shrek forever after', 'Puss in boots'];
+
+const addmarvel = () => {
+marvels.push("TOETER");
+}
+
+addmarvel()
+
+for (let marvel of marvels){
+    
+console.log(marvel);
+}
+
+
